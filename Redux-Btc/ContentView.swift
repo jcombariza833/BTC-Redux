4699ba932b852bf6fc2a057fd92083fcaef650ab
Juan Pablo Combariza <jp.combariza833@gmail.com>
2021-05-05 13:30:42 -0400

    Initial Commit

---
+++ Redux-Btc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Redux-Btc
+//
+//  Created by JUAN PABLO COMBARIZA MEJIA on 5/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Redux-Btc/Redux_BtcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_BtcApp.swift
+//  Redux-Btc
+//
+//  Created by JUAN PABLO COMBARIZA MEJIA on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Redux_BtcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
